65c712aa027e69183adb78fbd38d40b90244ff9c
Sujeet <sujeet4work@gmail.com>
2023-11-03 16:07:25 -0700

    app

---
+++ index.js
@@ -0,0 +1,10 @@
+// index.js
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req, res) => res.send('Hello TeamCity!'));
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
